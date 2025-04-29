b12607de0ec0bc4e342ebcb5346d0298bd8c9ca4
syq <951775089@qq.com>
2025-04-29 14:42:47 +0800

    换成了真实的

--- page/baidu_page.py
+++ page/baidu_page.py
@@ -7,3 +7,13 @@ class BaiduPage(Page):
     settings = Element(css="#s-usersetting-top", describe="设置")
     search_setting = Element(css="#s-user-setting-menu > div > a.setpref", describe="搜索设置")
     save_setting = Element(link_text="保存设置", describe="保存设置")
+
+
+class LgPage(Page):
+    login_account = Element(id_="login_userName", describe="用户名")
+
+
+
+    login_pwd = Element(id_="login_password", describe="密码")
+    login_code = Element(css = "#login_validateCode > span > input", describe="验证码")
+    login_login_button = Element(css = "#login > button", describe="登录按钮")
---
+++ test_dir/test_login.py
@@ -0,0 +1,44 @@
+# 2025/4/29 13:43
+# -*- coding:UTF-8 -*-
+
+import sys
+from time import sleep
+import pytest
+from os.path import dirname, abspath
+from page.baidu_page import BaiduPage, LgPage
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+sys.path.insert(0, dirname(dirname(abspath(__file__))))
+
+
+class TestLogin:
+    """登录"""
+
+    def test_login_success_case(self, browser, base_url):
+        """
+        名称：使用正确账密登录
+        步骤：
+        1、打开浏览器
+        2、输入正确账密
+        3、点击登录按钮
+        检查点：
+        * 检查页面标题是否包含关键字。
+        """
+        page = LgPage(browser)
+        page.open(base_url)
+        sleep(5)
+        page.login_account = "qianchuan"
+        page.login_pwd = "Qianchuan@123"
+        page.login_code = "1111"
+        page.login_login_button.click()
+        sleep(5)
+
+        assert browser.title == "柳工大模型应用平台"
+
+
+
+#
+# if __name__ == '__main__':
+#     pytest.main(["-v", "-s", "test_login.py"])
+
